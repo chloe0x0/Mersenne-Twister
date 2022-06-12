b0abaf499dfdd52a336579a48b0b82becdd43c36
chloe0b0 <84647960+chloe0b0@users.noreply.github.com>
2022-06-11 23:16:06 -0700

    Update MT19937.c

--- src/MT19937.c
+++ src/MT19937.c
@@ -53,8 +53,8 @@ unsigned long MT_GenNat(MT19937* mt){
     y = mt->state[mt->index++];
     y ^= (y >> 11 );
     y ^= (y << 7  ) & T_MASK_B;
-    y ^= (y >> 15 ) & T_MASK_C;
-    y ^= (y << 18 );
+    y ^= (y << 15 ) & T_MASK_C;
+    y ^= (y >> 18 );
 
     return y;
 }
